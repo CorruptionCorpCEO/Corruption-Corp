07650e1e4fb78350954367509ee42e920fde0fe1
Corruption Corp <158696972+CorruptionCorpCEO@users.noreply.github.com>
2026-09-04 01:52:20 +1000

    Deploy site - 2026-09-03 15:52 UTC (source f2f9531)

--- assets/ArtistId-w0PznSpb.js
+++ assets/ArtistId-w0PznSpb.js
@@ -1,4 +1,4 @@
-import{r as x,j as c}from"./index-B8yKGzyI.js";import{a as D}from"./artistData-_FHshtW1.js";import{getCachedOverride as q,fetchArtistOverride as G}from"./artistOverrides-DNFAJiew.js";import{u as $}from"./touchScroll-CNpHYOsU.js";import"./profile-BBKalGiG.js";import"./firebase-D4vUOye5.js";const Y="#1414dc",O=a=>{const u=(a||"").replace("#","");return u.length!==6?null:[0,2,4].map(t=>parseInt(u.substr(t,2),16)/255)},X=a=>a||Y,V=`
+import{r as x,j as c}from"./index-BBORVbGo.js";import{a as D}from"./artistData-_FHshtW1.js";import{getCachedOverride as q,fetchArtistOverride as G}from"./artistOverrides-DNFAJiew.js";import{u as $}from"./touchScroll-CebjTz7H.js";import"./profile-BBKalGiG.js";import"./firebase-D4vUOye5.js";const Y="#1414dc",O=a=>{const u=(a||"").replace("#","");return u.length!==6?null:[0,2,4].map(t=>parseInt(u.substr(t,2),16)/255)},X=a=>a||Y,V=`
 attribute vec2 aPos;
 void main() { gl_Position = vec4(aPos, 0.0, 1.0); }
 `,K=`
